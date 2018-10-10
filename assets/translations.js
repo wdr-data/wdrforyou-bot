ecde81bb469b91dcf8baded9430c170032123c69
Jannes Höke <jhoeke@uni-bremen.de>
2018-10-10 15:23:10 +0200

    :art: Improve syntax for translation file

--- assets/translations.js
+++ assets/translations.js
@@ -1,14 +1,14 @@
 export default {
-    "subscriptionIntro": {
-        "arabic": "اختر لغتك المفضلة",
-        "persian": "فارسی",
-        "english": "Choose your language",
-        "german": "Wähle deine Sprache"
+    subscriptionIntro: {
+        arabic: "اختر لغتك المفضلة",
+        persian: "فارسی",
+        english: "Choose your language",
+        german: "Wähle deine Sprache"
     },
-    "subscriptionReturn": {
-        "arabic": "مرحبا بكم في WICHTIGforyou. هنا تحصلون من \"في دي إر فوريو\" على آخر الأخبار العاجلة من خلال تطبيق الميسنجر. من خلالنا تحصل على آخر الأخبار الموثوقة باللغة العربية.",
-        "persian": "سلام از \"WICHTIGforyou\". در اینجا \"وی دی ارفوریو\" مهمترین اخبار تازه و اطلاعات پشت صحنه را به طور مستقیم و سریع در مسنجر شما میفرستد. باما، اخبار  موثق و تاییده شده را به زبان های فارسی-آلمانی دریافت می کنید.",
-        "english": "Hey there. This is WICHTIGforyou, the place where WDRforyou provides you with Breaking NEWS  and background information directly and promptly via Messenger. Here you receive reliable and well researched News in English and German.",
-        "german": "Hallo bei WICHTIGforyou. Hier schickt Dir WDRforyou die wichtigsten Eilmeldungen und Hintergrundinformation direkt und schnell über den Messenger. Bei uns bekommst Du verlässliche und gut recherchierte Nachrichten in Englisch-Deutsch."
-    }
+    subscriptionReturn: {
+        arabic: "مرحبا بكم في WICHTIGforyou. هنا تحصلون من \"في دي إر فوريو\" على آخر الأخبار العاجلة من خلال تطبيق الميسنجر. من خلالنا تحصل على آخر الأخبار الموثوقة باللغة العربية.",
+        persian: "سلام از \"WICHTIGforyou\". در اینجا \"وی دی ارفوریو\" مهمترین اخبار تازه و اطلاعات پشت صحنه را به طور مستقیم و سریع در مسنجر شما میفرستد. باما، اخبار  موثق و تاییده شده را به زبان های فارسی-آلمانی دریافت می کنید.",
+        english: "Hey there. This is WICHTIGforyou, the place where WDRforyou provides you with Breaking NEWS  and background information directly and promptly via Messenger. Here you receive reliable and well researched News in English and German.",
+        german: "Hallo bei WICHTIGforyou. Hier schickt Dir WDRforyou die wichtigsten Eilmeldungen und Hintergrundinformation direkt und schnell über den Messenger. Bei uns bekommst Du verlässliche und gut recherchierte Nachrichten in Englisch-Deutsch."
+    },
 }
