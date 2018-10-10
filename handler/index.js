8da1573ebfd6b34b91d4bfc7898b51116ea9529c
LizzAy <lisachenbach@gmx.de>
2018-10-10 12:39:35 +0200

    :bug: Use correct payload for button

--- handler/index.js
+++ handler/index.js
@@ -3,7 +3,7 @@ export default {
         'current_time': require('./actionCurrentTime').default,
         'latest_reports': require('./payloadLatestReports').default,
         'report': require('./payloadReport').handler,
-        'subscriptionList': require('./payloadSubscribe').subscriptionList,
+        'subscriptions': require('./payloadSubscribe').subscriptionList,
         'subscribe': require('./payloadSubscribe').subscribe,
     },
 };
