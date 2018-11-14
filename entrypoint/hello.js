a0d50201b25543f05b2dfc6ec29e90513dc873c3
Jannes Höke <jhoeke@uni-bremen.de>
2018-11-14 14:59:59 +0100

    :alembic: Do some more experimentation

--- entrypoint/hello.js
+++ entrypoint/hello.js
@@ -1,19 +1,22 @@
 'use strict';
 
 module.exports.hello = async (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Go Serverless v1.0! Your function executed successfully! ${process.env['TEST']}`,
-      input: event,
-    }),
-  };
 
-  callback(null, response);
+    callback({
+        statusCode: 200,
+        body: JSON.stringify({
+            message: `Go Serverless v1.0! Your function executed successfully! ${process.env['TEST']}`,
+            input: event,
+        }),
+    });
 
-  await (async () => {
-      throw new Error("AHHHHH");
-    })()
+    try {
+        await (async () => {
+            throw new Error("AHHHHH");
+        })()
+    } catch (e) {
+        console.error(e);
+    }
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
