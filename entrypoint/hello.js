e4a95829a9a33f7da5395b6605279f9749e42985
Jannes Höke <jhoeke@uni-bremen.de>
2018-11-14 15:16:38 +0100

    :alembic: Arbitrary shit

--- entrypoint/hello.js
+++ entrypoint/hello.js
@@ -1,23 +1,20 @@
 'use strict';
 
-module.exports.hello = async (event, context, callback) => {
-
-    callback({
-        statusCode: 200,
-        body: JSON.stringify({
-            message: `Go Serverless v1.0! Your function executed successfully! ${process.env['TEST']}`,
-            input: event,
-        }),
-    });
+module.exports.hello = async (event, context) => {
 
     try {
         await (async () => {
             throw new Error("AHHHHH");
         })()
     } catch (e) {
-        console.error(e);
+        console.error('Arbritary schit', e);
     }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: `Go Serverless v1.0! Your function executed successfully! ${process.env['TEST']}`,
+            input: event,
+        }),
+    };
 };
