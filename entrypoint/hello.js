123dedb35ddabada80d36f62b4abff249e5b90f9
Jannes Höke <jhoeke@uni-bremen.de>
2018-11-14 14:43:17 +0100

    :alembic: Raise async error for testing purposes

--- entrypoint/hello.js
+++ entrypoint/hello.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.hello = async (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -10,6 +10,10 @@ module.exports.hello = (event, context, callback) => {
   };
 
   callback(null, response);
+
+  await (async () => {
+      throw new Error("AHHHHH");
+    })()
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
