7c6124574beb2f605b88d05131a2a0d8274d09f1
LizzAy <lisachenbach@gmx.de>
2018-11-15 15:21:32 +0100

    :lipstick: Change image of link to WDRforyou fb-site

--- handler/payloadLink.js
+++ handler/payloadLink.js
@@ -4,7 +4,7 @@ export const linkWdrForYou = async function (chat) {
     const element = genericElement(
         'Facebook WDRforyou',
         null,
-        'https://s3.eu-central-1.amazonaws.com/newsforyou-bot-assets-jhoeke/Deutsch_D1.png',
+        'https://scontent-ber1-1.xx.fbcdn.net/v/t1.0-9/12592485_830120313764025_6677887399999214391_n.png?_nc_cat=1&_nc_ht=scontent-ber1-1.xx&oh=b534e82d9e46d52619fd64961feb5b6b&oe=5C774115',
         [ buttonUrl('WDRforyou', 'https://www.facebook.com/WDRforyou/', 'full') ],
     );
 
