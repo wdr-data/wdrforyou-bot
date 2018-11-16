a878fcc806837b8c15baa02a6c49ade70c8743fb
Jannes Höke <jhoeke@uni-bremen.de>
2018-11-16 12:47:35 +0100

    :bug: Handle errors in broadcasting hook

--- entrypoint/cms.js
+++ entrypoint/cms.js
@@ -5,60 +5,67 @@ import { makeMoreButton } from "../handler/payloadReport";
 import translations from "../assets/translations";
 
 
-export const sendReport = async (event, context, callback) => {
-    const payload = JSON.parse(event.body);
+export const sendReport = async (event, context) => {
+    try {
+        const payload = JSON.parse(event.body);
 
-    callback(null, {
-        statusCode: 200,
-        body: 'ok',
-    });
+        console.log(JSON.stringify(payload, null, 2));
 
-    console.log(JSON.stringify(payload, null, 2));
+        const reportID = payload.id;
+        const url = `${urls.report(reportID)}`;
 
-    const reportID = payload.id;
-    const url = `${urls.report(reportID)}`;
-
-    const report = await request({ uri: url, json: true });
+        const report = await request({uri: url, json: true});
 
 
-    if (!report) {
-        console.error(`Report ${reportID} could not be retrieved from API`);
-        return;
-    }
-
-    console.log('Sending report: ' + JSON.stringify(report, null, 2));
-
-    let moreButton = makeMoreButton(translations.reportMoreButton.german, report);
-
-    let linkButton;
-
-    if (report.link) {
-        linkButton = buttonUrl(`🔗 Link`, report.link);
-    }
-
-    let buttons = [linkButton, moreButton].filter(e => !!e);
-
-    if (!buttons.length) {
-        console.log('Sending broadcast without buttons');
-        // Always send german text to german-subscribers
-        await sendBroadcastText(report.text, null, 'german');
-
-        for (const translation of report.translations) {
-            await sendBroadcastText(`${translation.text}\n\n${report.text}`, null, translation.language);
+        if (!report) {
+            console.error(`Report ${reportID} could not be retrieved from API`);
+            return;
         }
-    } else {
-        console.log('Sending broadcast with buttons');
-        // Always send german text to german-subscribers
-        await sendBroadcastButtons(report.text, buttons, null, 'german');
 
-        for (const translation of report.translations) {
-            moreButton = makeMoreButton(translations.reportMoreButton[translation.language], report);
-            buttons = [linkButton, moreButton].filter(e => !!e);
-            await sendBroadcastButtons(`${translation.text}\n\n${report.text}`, buttons, null, translation.language);
+        console.log('Sending report: ' + JSON.stringify(report, null, 2));
+
+        let moreButton = makeMoreButton(translations.reportMoreButton.german, report);
+
+        let linkButton;
+
+        if (report.link) {
+            linkButton = buttonUrl(`🔗 Link`, report.link);
         }
-    }
 
-    return markSent(reportID);
+        let buttons = [linkButton, moreButton].filter(e => !!e);
+
+        if (!buttons.length) {
+            console.log('Sending broadcast without buttons');
+            // Always send german text to german-subscribers
+            await sendBroadcastText(report.text, null, 'german');
+
+            for (const translation of report.translations) {
+                await sendBroadcastText(`${translation.text}\n\n${report.text}`, null, translation.language);
+            }
+        } else {
+            console.log('Sending broadcast with buttons');
+            // Always send german text to german-subscribers
+            await sendBroadcastButtons(report.text, buttons, null, 'german');
+
+            for (const translation of report.translations) {
+                moreButton = makeMoreButton(translations.reportMoreButton[translation.language], report);
+                buttons = [linkButton, moreButton].filter(e => !!e);
+                await sendBroadcastButtons(`${translation.text}\n\n${report.text}`, buttons, null, translation.language);
+            }
+        }
+
+        await markSent(reportID);
+        return {
+            statusCode: 200,
+            body: 'ok',
+        }
+    } catch (e) {
+        console.error('Error:', e);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(e, null, 2),
+        };
+    }
 };
 
 const markSent = async (id) => {
