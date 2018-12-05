3eed9b4754c6c81f44f597d100c7b3fd9a1ef4b4
LizzAy <lisachenbach@gmx.de>
2018-12-05 13:58:37 +0100

    :sparkles: Add script to send Video Template Broadcast

---
+++ sendVideoBroadcast.js
@@ -0,0 +1,23 @@
+import { sendBroadcastMediaTemplate, buttonPostback } from "./lib/facebook";
+import { uploadAttachment } from "./lib/facebookAttachments";
+
+async function send(videoUrl) {
+    const attachmentID = await uploadAttachment(
+        videoUrl,
+        'video',
+        100000
+    )
+    console.log(attachmentID);
+
+    await sendBroadcastMediaTemplate(
+        'video',
+        attachmentID,
+        [buttonPostback(
+            'Anmelden',
+            { action: 'subscriptions'}
+        )],
+        true
+    )
+}
+
+send(process.argv[2])
