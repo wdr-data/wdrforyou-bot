4414ee43f050edcad09dd08d2bc9062a27e9edcc
Jannes Höke <jhoeke@uni-bremen.de>
2019-03-07 11:03:01 +0100

    :children_crossing: :art: Add check for analytics choice

--- entrypoint/fb.js
+++ entrypoint/fb.js
@@ -79,12 +79,8 @@ const sendDefaultReply = async (chat) => {
         const lastReply = await lastDefaultReplies.load(chat.psid);
         sendReply = lastReply.ttl <= Math.floor(Date.now() / 1000);
     } catch {
-        const subscriptions = new DynamoDbCrud(process.env.DYNAMODB_SUBSCRIPTIONS)
-        if (await subscriptions.load(chat.psid)) {
-            sendReply = true;
-        } else {
-            sendReply = false;
-        }
+        const tracking = new DynamoDbCrud(process.env.DYNAMODB_TRACKING);
+        sendReply = chat.subscribed || await tracking.load(chat.psid);
     }
 
     if (sendReply) {
