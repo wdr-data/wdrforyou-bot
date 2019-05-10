37e7ba50cb83cca18d6a6393fbb859f5ddfd083c
ChristianJoe <cjoe@gmx.de>
2019-05-10 10:33:36 +0200

    :children_crossing: Pause between video and subscription list in onboarding

--- handler/payloadGetStarted.js
+++ handler/payloadGetStarted.js
@@ -1,8 +1,10 @@
 import videos from '../assets/videos';
 import { subscriptionList } from './payloadSubscribe';
+import { snooze } from '../lib/util';
 
 export const handler = async (chat) => {
     await chat.sendAttachment(chat.getTranslation(videos.getStarted));
+    await snooze(5000);
     return subscriptionList(chat);
 };
 
