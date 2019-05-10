a514892e01229ba4b0112d1aed11ea06d8a1b957
ChristianJoe <cjoe@gmx.de>
2019-05-10 17:37:54 +0200

    :chart_with_upwards_trend: Track if help is needed for subscription

--- handler/payloadSubscriptionHelp.js
+++ handler/payloadSubscriptionHelp.js
@@ -1,6 +1,9 @@
 import videos from '../assets/videos';
 
 export const handler = async (chat) => {
+    if (chat.trackingEnabled) {
+        await chat.track.event('Subscription', 'HelpVideo', chat.language).send();
+    }
     return chat.sendAttachment(chat.getTranslation(videos.getStarted));
 };
 
